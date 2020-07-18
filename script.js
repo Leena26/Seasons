d03f363b5849b594d6b29a65f2964b0fab1ce4a3
Leena26 <67622202+Leena26@users.noreply.github.com>
2020-07-18 21:25:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function summer(){
+    alert("Hey, It's Summer!");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')";
+    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundRepeat = "no-repeat"
+}
+function autumn(){
+    alert("Hey, It's Autumn!");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backgroundSize = "100%";
+}
+function winter(){
+    alert("Hey, It's Winter!");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+    document.body.style.backgroundSize = "100%";
+}
+function spring(){
+    alert("Hey, It's Spring!");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+    document.body.style.backgroundSize = "100%";
+}
+function copy(){
+    alert("LOL! you cant copy! ")
+}
